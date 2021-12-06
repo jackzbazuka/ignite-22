0121212119d614aa61d8c929b20e6a453f71998d
jackzbazuka <milindmoto99@gmail.com>
2021-12-06 10:38:46 +0530

    modified home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
+import NextLink from 'next/link'
 import { useEffect, useState } from 'react'
-import Layout from '@/layouts/Layout'
-import Loading from '@/components/Loading'
 import { initialize } from '@/lib/clientApp'
 import * as gtag from '@/lib/gTag'
 
@@ -32,8 +31,16 @@ export default function App({ Component, pageProps }) {
 		}
 	}, [])
 
+	const NavLink = ({ text, link }) => {
+		return (
+			<NextLink href={link}>
+				<a className='p-1'>{text}</a>
+			</NextLink>
+		)
+	}
+
 	return (
-		<div className='flex flex-row'>
+		<div className='flex flex-col'>
 			<Script
 				strategy='afterInteractive'
 				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
@@ -52,6 +59,21 @@ export default function App({ Component, pageProps }) {
           `,
 				}}
 			/>
+			<header className='sticky z-10 top-0 h-28 md:h-24 lg:h-20 w-full py-2 md:py-1 flex flex-row justify-between items-center text-sm bg-cyan-darker'>
+				{router.pathname != '/' ? (
+					<img src='/logo/fire.svg' className='h-14' />
+				) : (
+					<div className='h-1 w-1'></div>
+				)}
+				<nav className='flex flex-row justify- items-center gap-x-8'>
+					<NavLink text='Events' link='/' />
+					<NavLink text='Sponsors' link='/' />
+					<NavLink text='About' link='/' />
+					<button className='px-6 py-2 font-semibold text-black bg-yellow hover:opacity-90 rounded transition-all'>
+						Join
+					</button>
+				</nav>
+			</header>
 			<Component {...pageProps} />
 		</div>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,45 @@
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+	const today = new Date(2021, 12, 5)
+	const endDate = new Date(2022, 3, 20)
+
+	const days = Math.floor((endDate - today) / (1000 * 60 * 60 * 24))
+	const weeks = Math.floor(days / 7)
+	const hours = parseInt((Math.abs(endDate - today) / (1000 * 60 * 60)) % 24)
+	const minutes = parseInt(
+		(Math.abs(endDate.getTime() - today.getTime()) / (1000 * 60)) % 60
+	)
+
+	useEffect(() => {
+		console.log(
+			`Days: ${days} Weeks: ${weeks} Hours: ${hours} Minutes: ${minutes}`
+		)
+	})
+
 	return (
-		<div className='mt-60 w-full grid place-items-center bg-transparent'>
-			<div className='flex flex-row'>
+		<div className='mt-14 w-full grid place-items-center bg-transparent'>
+			<div className='flex flex-col lg:flex-row'>
 				<object
 					type='image/svg+xml'
 					data='/logo/animated-fire.svg'
-					className='h-72 bg-transparent'
+					className='h-48 md:h-56 lg:h-64 bg-transparent'
 				/>
 				<object
 					type='image/svg+xml'
 					data='/logo/text.svg'
-					className='h-52 self-end bg-transparent'
+					className='h-24 md:h-32 lg:h-42 self-end bg-transparent'
 				/>
+			</div>
+			<span className='mt-5 font-bold text-5xl lining-nums'>
+				{`00`}:{`29`}:{`00`}:{`32`}
+			</span>
+			<div className='mt-20 w-full'>
+				<div className='mx-auto h-2 w-3/4 flex flex-row justify-around items-center bg-cyan rounded-md'>
+					<div className='h-5 w-5 bg-white rounded-full' />
+					<div className='h-5 w-5 bg-white rounded-full' />
+					<div className='h-5 w-5 bg-white rounded-full' />
+				</div>
 			</div>
 		</div>
 	)
