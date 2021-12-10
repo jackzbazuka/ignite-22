4d81859dcdb9ae91d7705b2dbd6d09bc18c05631
jackzbazuka <milindmoto99@gmail.com>
2021-12-10 23:49:41 +0530

    removed unused components

--- components/Loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-	return (
-		<div className='h-screen w-full grid place-items-center text-white bg-gray-900'>
-			<span>Loading</span>
-		</div>
-	)
-}
--- components/NavBar.js
+++
@@ -1,3 +0,0 @@
-export default function NavBar() {
-	return <div className='w-full'></div>
-}
