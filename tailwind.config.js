2e8ffe306a5430e13d73c90312c7e2430fca0f3f
jackzbazuka <milindmoto99@gmail.com>
2021-12-10 23:47:35 +0530

    modified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	mode: 'jit',
-	purge: [
+	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
-	darkMode: false, // or 'media' or 'class'
 	theme: {
 		colors: {
 			'current': 'currentColor',
 			'transparent': 'transparent',
 			'white': colors.white,
-			'black': '#01171C',
-			'soft-red': '#F14956',
-			'yellow': '#F2E206',
-			'cyan': {
-				lighter: '#35BFB0',
-				light: '#19736A',
-				DEFAULT: '#175260',
-				dark: '#092C34',
-				darker: '#011F27',
-			},
+			'black': colors.black,
+			'rich-black': '#040C0E',
+			'jungle-green': '#132227',
+			'feldgrau': '#525B56',
+			'shadow': '#87776C',
+			'coffee': '#714D35',
+			...colors,
 		},
 		extend: {
 			backgroundImage: {
 				'gradient-radial':
 					'radial-gradient(115% 90% at 100% 100%, var(--tw-gradient-stops))',
+				'bg01': "url('/img/hero-pattern.svg')",
+				'bg02': "url('/img/hero-pattern.svg')",
+				'bg03': "url('/img/hero-pattern.svg')",
+				'bg04': "url('/img/hero-pattern.svg')",
 			},
 		},
 	},
